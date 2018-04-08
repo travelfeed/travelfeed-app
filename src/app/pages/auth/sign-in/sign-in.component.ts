9b2b515dbf7d827c9437730cbd1373382d15e034
Pascal Iske <info@pascal-iske.de>
2018-04-08 22:11:01 +0200

    fix(sign-in): fix sign in form component

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Router } from '@angular/router'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { takeWhile } from 'rxjs/operators'
 import { AuthService } from '../auth.service'
+import { SignInFormModel } from './typings'
 
 @Component({
     selector: 'cmp-sign-in',
     templateUrl: './sign-in.component.html',
     styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit, OnDestroy {
     public signInForm: FormGroup
+
+    public model: SignInFormModel = {
+        email: '',
+        password: ''
+    }
+
+    private alive: boolean = true
 
     public constructor(
         private router: Router,
         private formBuilder: FormBuilder,
         public authService: AuthService
-    ) {}
+    ) {
+        this.signInForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(30),
+                    Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9<!{,=~)]+)$')
+                ]
+            ]
+        })
+    }
 
     public ngOnInit(): void {
         this.authService.isSignedIn().subscribe((authenticated: boolean) => {
@@ -23,18 +45,28 @@ export class SignInComponent implements OnInit {
                 this.router.navigate(['../backend'])
             }
         })
+    }
 
-        this.signInForm = this.formBuilder.group({
-            email: ['', Validators.required],
-            password: ['', Validators.required]
-        })
+    public ngOnDestroy(): void {
+        this.alive = false
+    }
+
+    public get email() {
+        return this.signInForm.get('email')
+    }
+
+    public get password() {
+        return this.signInForm.get('password')
     }
 
     public signin(): void {
         const { email, password } = this.signInForm.value
 
-        this.authService.signin(email, password).subscribe(result => {
-            this.router.navigate(['/auth'])
-        })
+        this.authService
+            .signin(email, password)
+            .pipe(takeWhile(() => this.alive))
+            .subscribe(result => {
+                this.router.navigate(['/auth'])
+            })
     }
 }
