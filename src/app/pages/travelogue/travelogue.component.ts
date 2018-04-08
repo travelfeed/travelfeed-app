eb6f9e8e6c319fc19b84029344457d309536fd78
DennisFritschDE <de.fritsch@web.de>
2018-04-08 19:01:49 +0200

    Import AgmCoreModule

--- src/app/pages/travelogue/travelogue.component.ts
+++ src/app/pages/travelogue/travelogue.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./travelogue.component.scss']
 })
 export class TravelogueComponent implements OnInit {
+    lat: number = 51.678418
+    lng: number = 7.809007
+
     constructor() {}
 
     ngOnInit() {}
--- src/app/pages/travelogue/travelogue.module.ts
+++ src/app/pages/travelogue/travelogue.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TravelogueComponent } from './travelogue.component'
+import { AgmCoreModule } from '@agm/core'
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, AgmCoreModule],
     declarations: [TravelogueComponent]
 })
 export class TravelogueModule {}
