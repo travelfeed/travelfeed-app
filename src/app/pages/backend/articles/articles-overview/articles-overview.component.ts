daae317832ff807660b4f9c8852e1176558fb55d
Pascal Iske <info@pascal-iske.de>
2018-04-12 15:23:39 +0200

    feat(articles): add articles service, overview and details components

---
+++ src/app/pages/backend/articles/articles-overview/articles-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'cmp-articles-overview',
+    templateUrl: './articles-overview.component.html',
+    styleUrls: ['./articles-overview.component.scss']
+})
+export class ArticlesOverviewComponent implements OnInit {
+    public constructor() {}
+
+    public ngOnInit() {}
+}
