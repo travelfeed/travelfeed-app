7d2a0d89515c21a556636eba41e810e5d7c5687b
Pascal Iske <info@pascal-iske.de>
2018-05-16 00:50:42 +0200

    fix(backend): update styles, add page headlines

--- src/app/pages/backend/articles/articles.module.ts
+++ src/app/pages/backend/articles/articles.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core'
 import { SharedModule } from '../../../shared/shared.module'
 import { FormElementsModule } from '../../../components/form-elements/form-elements.module'
 import { AppRoutingModule } from '../../../app-routing.module'
+import { HeadlinesModule } from '../../../components/headlines/headlines.module'
 import { ArticlesComponent } from './articles.component'
 import { ArticlesListComponent } from './articles-list/articles-list.component'
 import { ArticlesDetailsComponent } from './articles-details/articles-details.component'
 import { ArticlesService } from './articles.service'
 
 @NgModule({
-    imports: [SharedModule, FormElementsModule, AppRoutingModule],
+    imports: [SharedModule, FormElementsModule, AppRoutingModule, HeadlinesModule],
     declarations: [ArticlesComponent, ArticlesListComponent, ArticlesDetailsComponent],
     providers: [ArticlesService],
 })
--- src/app/pages/backend/users/users.module.ts
+++ src/app/pages/backend/users/users.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AppRoutingModule } from '../../../app-routing.module'
+import { HeadlinesModule } from '../../../components/headlines/headlines.module'
 import { UsersComponent } from './users.component'
 import { UsersListComponent } from './users-list/users-list.component'
 import { UsersDetailsComponent } from './users-details/users-details.component'
 import { UsersService } from './users.service'
 
 @NgModule({
-    imports: [CommonModule, AppRoutingModule],
+    imports: [CommonModule, AppRoutingModule, HeadlinesModule],
     declarations: [UsersComponent, UsersListComponent, UsersDetailsComponent],
     providers: [UsersService],
 })
