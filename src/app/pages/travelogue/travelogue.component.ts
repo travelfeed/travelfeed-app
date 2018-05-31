15bd44fac2a13eb1d8b5ecbf4ed9be5938ef6a32
Pascal Iske <info@pascal-iske.de>
2018-05-31 11:24:54 +0200

    fix(travelogue): temporary disable max-line-length until data is fetched from api

--- src/app/pages/travelogue/travelogue.component.ts
+++ src/app/pages/travelogue/travelogue.component.ts
@@ -14,6 +14,7 @@ export class TravelogueComponent {
 
     public commentfield: FormControl = new FormControl('')
 
+    /* tslint:disable:max-line-length */
     public text: string = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
     sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
@@ -34,6 +35,7 @@ export class TravelogueComponent {
     sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
     At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`
+    /* tslint:enable:max-line-length */
 
     public constructor() {}
 }
