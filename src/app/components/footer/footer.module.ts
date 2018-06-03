4ea2a3b7b92fcb38d21a71f5078068289ff20a81
DennisFritschDE <de.fritsch@web.de>
2018-06-03 15:09:19 +0200

    Languageselect hinzugefügt

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -1,10 +1,11 @@
+import { FormElementsModule } from './../form-elements/form-elements.module'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FooterComponent } from './footer.component'
 import { SharedModule } from '../../shared/shared.module'
 
 @NgModule({
-    imports: [CommonModule, SharedModule],
+    imports: [CommonModule, SharedModule, FormElementsModule],
     declarations: [FooterComponent],
     exports: [FooterComponent],
 })
--- src/app/components/form-elements/f-select/f-select.component.ts
+++ src/app/components/form-elements/f-select/f-select.component.ts
@@ -14,6 +14,8 @@ export class FSelectComponent extends FInputComponent {
 
     @Input() public options: Array<FSelectOption> = []
 
+    @Input() public bgcolor: string = ''
+
     public getClasses(baseClass: string): object {
         const prefixed = modifier => `${baseClass}--${modifier}`
         const results = super.getClasses(baseClass)
